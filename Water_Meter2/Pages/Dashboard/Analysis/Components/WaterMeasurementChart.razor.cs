ebb6573a1fe075c95d1fa0ff500385d3f9864c7e
Enrique Carbonell Enriquez <enrique_carbonel@hotmail.com>
2023-04-03 23:21:49 -0600

    Se hicieron cambios para poder agregar el cambio del tiempo en la grafica semanal. Se creo una clase y un componente blazor

---
+++ Water_Meter2/Pages/Dashboard/Analysis/Components/WaterMeasurementChart.razor.cs
@@ -0,0 +1,36 @@
+﻿using AntDesign.Charts;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
+using Water_Meter2.Services;
+
+namespace Water_Meter2.Pages.Dashboard.Analysis
+{
+    public partial class WaterMeasurementChart
+    {
+
+        DateTime SelectedWeek;
+        [Inject] public IWaterChartService ChartService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            //await OnTabChanged("1");
+        }
+
+        //private async Task OnTabChanged(string activeKey)
+        //{
+        //    //var data = await ChartService.GetSalesDataAsync();
+        //    //if (activeKey == "1")
+        //    //    await _saleChart.ChangeData(data);
+        //    //else
+        //    //    await _visitChart.ChangeData(data);
+        //}
+
+        private void WeekChanged()
+        { 
+           
+        }
+
+    }
+}
