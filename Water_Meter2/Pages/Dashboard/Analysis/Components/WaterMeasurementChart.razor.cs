15a742eb2392642b75a34581bf3c22ccc18285f2
Enrique Carbonell Enriquez <enrique_carbonel@hotmail.com>
2023-04-05 18:48:46 -0600

    Se agrego la barra semanal del consumo del agua con bugs que no muestran los datos correctos ni la fecha correcta

--- Water_Meter2/Pages/Dashboard/Analysis/Components/WaterMeasurementChart.razor.cs
+++ Water_Meter2/Pages/Dashboard/Analysis/Components/WaterMeasurementChart.razor.cs
@@ -9,26 +9,61 @@ using Water_Meter2.Services;
 
 namespace Water_Meter2.Pages.Dashboard.Analysis
 {
-  public partial class WaterMeasurementChart
-  {  
-    DateTime StartDate;
-    DateTime EndDate;
-    
 
-    [Inject] 
-    public IWaterChartService WaterChartService { get; set; }
-
-    public async void GetWeeklyData(DateTimeChangedEventArgs e)
+    public partial class WaterMeasurementChart
     {
-      ChartDataItem[] Weeklydata;
-      StartDate = e.Date;
-      if (e.Date.DayOfWeek != DayOfWeek.Sunday)
-      {
-        StartDate = StartDate.AddDays(((int)e.Date.DayOfWeek) * -1);
-      }
-      EndDate = StartDate.AddDays(6);
-      Weeklydata = await WaterChartService.GetWeeklyMeasurementDataAsync(StartDate, EndDate);
-      var x = Weeklydata.Length;
+        DateTime StartDate;
+        DateTime EndDate;
+
+        private readonly ColumnConfig _WaterChartConfig = new ColumnConfig
+        {
+            Title = new AntDesign.Charts.Title
+            {
+                Visible = true,
+                Text = "Water Measurement"
+            },
+            ForceFit = true,
+            Padding = "auto",
+            XField = "x",
+            YField = "y"
+        };
+
+        private IChartComponent _waterChart;
+
+        [Inject]
+        public IWaterChartService WaterChartService { get; set; }
+
+        public async void GetWeeklyData(DateTimeChangedEventArgs e)
+        {
+            ChartDataItem[] Weeklydata;
+            StartDate = e.Date;
+            if (e.Date.DayOfWeek != DayOfWeek.Sunday)
+            {
+                StartDate = StartDate.AddDays(((int)e.Date.DayOfWeek) * -1);
+            }
+            EndDate = StartDate.AddDays(6);
+            Weeklydata = await WaterChartService.GetWeeklyMeasurementDataAsync(StartDate, EndDate);
+            await _waterChart.ChangeData(Weeklydata);
+        }
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            //StartDate = DateTime.Now;
+            //if (StartDate.DayOfWeek != DayOfWeek.Sunday)
+            //{
+            //    StartDate = StartDate.AddDays(((int)StartDate.DayOfWeek) * -1);
+            //}
+            //EndDate = StartDate.AddDays(6);
+            await OnTabChanged("1");
+        }
+
+        private async Task OnTabChanged(string activeKey)
+        {
+            var data = await WaterChartService.GetWeeklyMeasurementDataAsync(StartDate, EndDate);
+            if (activeKey == "1")
+                await _waterChart.ChangeData(data);
+        }
     }
-  }
 }
